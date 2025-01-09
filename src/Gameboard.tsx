0890b6e827e52c4d1e2e6a28edc2bdadaa93ef21
dennyrfernandez2004 <dennyrfernandez2004@gmail.com>
2025-01-09 20:22:12 +0000

    Board array now has the actual elements

--- src/Gameboard.tsx
+++ src/Gameboard.tsx
@@ -4,15 +4,16 @@ import { Tile } from "./tile";
 
 export const Gameboard = () => {
     const [board, setBoard] = useState<string[][]>(
-        Array.from({ length: 6 }, () => Array(7).fill(''))
+        Array.from({ length: 6 }, () => Array(7).fill(<Tile />))
     )
     console.log(board)
     return (
         <div className="gameboard">
+            hi
             {board.map((row) => {
                 return <div className="row">
                     {row.map((tile) => {
-                        return <Tile />
+                        return tile
                     })}
                 </div>
             } )}
