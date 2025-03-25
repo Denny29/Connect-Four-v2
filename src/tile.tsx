91e0e907ea86a211ae8f818687d4ca0daec604ae
dennyrfernandez2004 <dennyrfernandez2004@gmail.com>
2025-03-25 16:47:36 +0000

    Removing some unused variables.

--- src/tile.tsx
+++ src/tile.tsx
@@ -1,10 +1,9 @@
 type TileProps = {
     bgColor: string
     clickProp: () => void
-    loc?: string
 }
 
-export const Tile = ({bgColor, clickProp, loc}: TileProps) => {
+export const Tile = ({bgColor, clickProp}: TileProps) => {
     return <div role="button" tabIndex={1} className={`tile ${bgColor}`} onClick={clickProp}>
         <div className={`inner inner-${bgColor}`}>
         </div>
